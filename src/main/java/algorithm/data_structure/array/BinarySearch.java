8e75aed975043bdac4805402c3b9ff2bf28fd81a
GouPinYu <2446038186@qq.com>
2024-06-15 18:44:51 +0800

    算法-数据结构-数组-二分查找 day1 2024.6.15

---
+++ src/main/java/algorithm/data_structure/array/BinarySearch.java
@@ -0,0 +1,13 @@
+package algorithm.data_structure.array;
+
+/**
+ * Leetcode 704 二分查找
+ * 给定升序整型数组(nums)和目标整数(target)
+ * 若目标整数存在返回数组下标
+ * 否则返回 -1
+ * */
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        return -1;
+    }
+}
